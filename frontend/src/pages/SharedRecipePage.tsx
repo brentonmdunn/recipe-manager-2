81e1074b0c32b776df61bd7a7a47b5c3ca2c0191
bmdunn <brentondunn@yahoo.com>
2026-04-25 19:24:22 +0000

    feat: surface tag chips on home page and match tag names in search

--- frontend/src/pages/SharedRecipePage.tsx
+++ frontend/src/pages/SharedRecipePage.tsx
@@ -114,11 +114,6 @@ export default function SharedRecipePage() {
 
           {recipe.tags.length > 0 && (
             <div className="flex flex-wrap gap-2 mt-4">
-              {recipe.category && (
-                <span className="bg-gray-100 px-3 py-1 rounded-full text-sm text-gray-600">
-                  {recipe.category.name}
-                </span>
-              )}
               {recipe.tags.map((tag) => (
                 <span
                   key={tag.id}
